f715ac942310bbcb41e765d04d6d3eed56580997
Lee Jun Seok <lego3410@gmail.com>
2019-11-15 19:58:47 +0900

    Migrate to using Brewfile

--- Homebrew/install.sh
+++ Homebrew/install.sh
@@ -1,4 +1,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install $(cat packages.txt)
-brew cask install $(cat casks.txt)
+brew fetch --retry
+brew bundle
