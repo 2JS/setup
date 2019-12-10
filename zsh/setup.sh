6dc76df44e2a093ce50ef7db0c1f6003dce8ec41
Lee, Jun Seok <lego3410@gmail.com>
2019-12-11 01:47:47 +0900

    Add user notices

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+echo "Creating Symlink from $HOME/Developer/setup/zsh/zshrc to $HOME/.zshrc"
 ln -s $HOME/Developer/setup/zsh/zshrc $HOME/.zshrc
-# source $HOME/.zshrc
 
+echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
