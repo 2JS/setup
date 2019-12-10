fa6bb28bb175a19608e99c64927e67ea28d0881c
Jun Seok Lee <lego3410@gmail.com>
2019-12-11 01:44:59 +0900

    Rename files .zsh* to zsh*

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -s $HOME/Developer/setup/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/Developer/setup/zsh/zshrc $HOME/.zshrc
 # source $HOME/.zshrc
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
