2594554c7ff1a1af3951833f360cda347cdc87ed
Jun Seok Lee <lego3410@gmail.com>
2020-01-02 01:43:26 +0900

    Replace zsh-syntax-highlighting with fast-syntax-highlighting

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,7 +1,8 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+# zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zdharma/fast-syntax-highlighting"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 
