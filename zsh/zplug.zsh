b5e3d871281aa4f7aff43996fd6bc4725e04ee10
Jun Seok Lee <lego3410@gmail.com>
2020-01-02 01:30:23 +0900

    Introduce zplug zsh plugin manager

---
+++ zsh/zplug.zsh
@@ -0,0 +1,8 @@
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-autosuggestions"
+
+zplug load
