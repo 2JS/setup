6f6a0d8c2a1f1f57e2d98ae5ad74e2fc5d77ab25
Jun Seok Lee <lego3410@gmail.com>
2020-01-14 11:06:08 +0900

    Disable zsh tracking untracked files

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -57,7 +57,7 @@ DISABLE_UPDATE_PROMPT="true"
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+DISABLE_UNTRACKED_FILES_DIRTY="true"
 
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
