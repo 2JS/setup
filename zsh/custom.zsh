f2787c62709e3165966b9fb699e0b251a897e8b4
Jun Seok Lee <lego3410@gmail.com>
2020-01-18 14:23:03 +0900

    Replace peco with fzf

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -7,7 +7,7 @@ source $SETUP/zsh/functions/edit-command-line.zsh
 
 # load peco commandline history
 # ref: https://www.44bits.io/ko/post/incremental-search-tool-peco
-source $SETUP/zsh/functions/peco-history.zsh
+# source $SETUP/zsh/functions/peco-history.zsh
 
 # Setting GOPATH
 export GOPATH=$HOME/Developer/Go
@@ -27,3 +27,7 @@ POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=true
 
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_USE_ASYNC=true
+
+# fzf commandline fuzzy completion
+# ref: https://subicura.com/2017/11/22/mac-os-development-environment-setup.html#커맨드라인-애플리케이션
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
