845d3ae94626addc92713d49453e9dbc228d6337
Lee, Jun Seok <lego3410@gmail.com>
2020-05-06 21:44:24 +0900

    Add ~/.gitignore_global

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,5 +3,7 @@
 echo "Creating Symlink from $HOME/.setup/zsh/zshrc to $HOME/.zshrc"
 ln -fs $HOME/.setup/zsh/zshrc.zsh $HOME/.zshrc
 
+ln -fs $HOME/.setup/zsh/.gitignore_global $HOME/.gitignore/global
+
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
