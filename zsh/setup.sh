226f7dca20fc4815728b94a39923e4f38c338939
Lee, Jun Seok <lego3410@gmail.com>
2020-09-22 16:19:19 +0900

    Sync gitconfig

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,7 +3,6 @@
 echo "Creating Symlink from $HOME/.setup/zsh/zshrc to $HOME/.zshrc"
 ln -fs $HOME/.setup/zsh/zshrc.zsh $HOME/.zshrc
 
-ln -fs $HOME/.setup/zsh/.gitignore_global $HOME/.gitignore/global
 
 # install Oh My Zsh
 echo "Installing Oh My Zsh"
