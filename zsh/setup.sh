c5170f3bc96d8f63b1cfb3e291fd913e9100aa52
2JS <lego3410@gmail.com>
2020-09-22 15:35:42 +0900

    Remove oh-my-zsh
    and install powerlevel10k via homebrew

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -5,5 +5,3 @@ ln -fs $HOME/.setup/zsh/zshrc.zsh $HOME/.zshrc
 
 ln -fs $HOME/.setup/zsh/.gitignore_global $HOME/.gitignore/global
 
-echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -87,8 +87,6 @@ plugins=(
 	vscode
 )
 
-source $ZSH/oh-my-zsh.sh
-
 # User configuration
 
 # export MANPATH="/usr/local/man:$MANPATH"
