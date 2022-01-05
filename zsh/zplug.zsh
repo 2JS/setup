1897a3ca2f37d392b033b7175df232755f5e6a8d
Lee, Jun Seok <lego3410@gmail.com>
2022-01-05 11:45:25 +0900

    Update discontinued plugin

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -2,7 +2,7 @@ export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
 # zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "zdharma/fast-syntax-highlighting"
+zplug "zdharma-continuum/fast-syntax-highlighting"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 
