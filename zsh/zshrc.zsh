2d69b43d2389730f1297db887dec140d44c103d2
Lee, Jun Seok <junseok.lee@voyagerx.com>
2023-06-15 16:35:46 +0900

    Set LC_ALL in zsh/zshrc

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -125,6 +125,9 @@ export EDITOR='vim'
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
+
+export LC_ALL=C.UTF-8
+
 # load custom environment
 source $HOME/.setup/zsh/alias.zsh
 source $HOME/.setup/zsh/custom.zsh
