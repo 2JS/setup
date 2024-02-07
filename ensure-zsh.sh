42376e5b4f75fe420ea492c858367967732a9308
Lee, Jun Seok <lego3410@gmail.com>
2024-02-08 01:25:51 +0900

    Add `ensure-zsh.sh`

---
+++ ensure-zsh.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export SETUP_ROOT=${SETUP_ROOT:-$HOME/.setup}
+if command -v zsh >/dev/null 2>&1; then
+  echo "zsh is installed"
+  exit 0
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install zsh
+elif [ "$(uname)" == "Linux" ]; then
+  sudo apt-get install -y zsh
+else
+  echo "Unknown operating system"
+  # TODO: manually install zsh
+fi
