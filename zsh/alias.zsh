52b4be18db804323ee2c268dabf08f378b6a3d8e
Lee, Jun Seok <lego3410@gmail.com>
2024-02-11 01:45:36 +0900

    Remove docker related alias
    docker compose alias is duplicate with `docker-compose` zsh plugin.
    docker machine alias is not used.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,17 +1,3 @@
-# docker-compose shortcuts
-alias dco='docker-compose'
-alias dcb='docker-compose build'
-alias dce='docker-compose exec'
-alias dcps='docker-compose ps'
-alias dcr='docker-compose run'
-alias dcup='docker-compose up'
-alias dcdn='docker-compose down'
-alias dcl='docker-compose logs'
-alias dclf='docker-compose logs -f'
-
-# docker-machine shortcuts
-alias dma='docker-machine'
-
 # eza, powerful ls alternative
 alias ls='eza -h'
 alias la='eza -ah'
