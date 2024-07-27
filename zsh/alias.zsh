ce8db1544650307cf1c4e21a266f95b4e4bf397e
Lee, Jun Seok <lego3410@gmail.com>
2024-07-28 00:33:43 +0900

    Update `zsh/alias.zsh`

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -5,3 +5,8 @@ alias ll='eza -lh'
 alias l='eza -lha'
 
 alias o='open'
+
+alias tfa='terraform apply -parallelism 32'
+alias tfaa='terraform apply -parallelism 32 -auto-approve'
+alias tfd='terraform destroy -parallelism 32'
+alias tff='terraform fmt -recursive'
