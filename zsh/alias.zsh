7677b964af379fb6974cb07f3e265da7c947e1b8
Lee, Jun Seok <lego3410@gmail.com>
2024-09-19 16:06:29 +0900

    Replace `bin/gfod` w/ alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -12,3 +12,5 @@ alias tfaa='terraform apply -parallelism 100 -auto-approve'
 alias tfd='terraform destroy -parallelism 100'
 alias tff='terraform fmt -recursive'
 alias tfsl='terraform state list | sort'
+
+alias gfod='git fetch origin develop:develop'
