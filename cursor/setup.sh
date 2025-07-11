168b06d842dd9045d35b680748075b389759ab05
Lee, Jun Seok <lego3410@gmail.com>
2025-07-11 17:29:16 +0900

    feat: add cursor

---
+++ cursor/setup.sh
@@ -0,0 +1,2 @@
+ln -s ~/.setup/cursor/keybindings.json ~/Library/Application\ Support/Cursor/User/keybindings.json
+ln -s ~/.setup/cursor/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
