9b1901df5baa9b841bec9ae2b7ecbab1bfd37ef0
Lee, Jun Seok <lego3410@gmail.com>
2025-12-08 10:54:13 +0900

    feat: `PATH=$HOME/.local/bin:$PATH`

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -14,6 +14,7 @@ export GOPATH=$HOME/.go
 export PATH="$PATH:$GOPATH/bin"
 
 export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="$PATH:/opt/homebrew/opt/rustup/bin"
 export PATH="$PATH:$SETUP/bin"
