7ecf11154bd4b795d73a3b3b5b8bcc78acb2e218
Lee, Jun Seok <lego3410@gmail.com>
2025-12-19 12:23:26 +0900

    WIP

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -140,3 +140,9 @@ source $HOME/.setup/zsh/custom.zsh
 
 autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C /opt/homebrew/bin/terraform terraform
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/path.zsh.inc' ]; then . '/opt/homebrew/share/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/completion.zsh.inc' ]; then . '/opt/homebrew/share/google-cloud-sdk/completion.zsh.inc'; fi
