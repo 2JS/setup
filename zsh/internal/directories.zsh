3de0960267b89becbf1c2d85880882d72517fc3e
Lee, Jun Seok <lego3410@gmail.com>
2026-06-19 16:00:57 +0900

    feat(zsh): restore parent-directory aliases

--- zsh/internal/directories.zsh
+++ zsh/internal/directories.zsh
@@ -2,6 +2,13 @@ setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
 
+# Support quick parent-directory navigation from common-aliases.
+# (Kept local to avoid re-enabling the full plugin.)
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
 function d () {
   if [[ -n $1 ]]; then
     dirs "$@"
